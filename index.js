d70de9a242de0ba9c85c25019eda6c165b35fe14
Ross Kukulinski <ross@kukulinski.com>
2015-06-11 16:02:16 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var http = require('http');
+var socketio = require('socket.io');
+var iostream = require('socket.io-stream');
+var fs = require('fs');
+var path = require('path');
+var Busboy = require('busboy');
+
+var app = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    var timeStart = new Date();
+    var busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      var filename = 'post_test_' + counter + '_' + filename;
+      file.on('end', function() {
+        var timeEnd = new Date();
+        var duration = timeEnd - timeStart;
+        console.log('HTTP streaming upload complete in ' + duration + 'ms');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('okay');
+        fs.unlink(filename, function() {});
+      });
+      file.pipe(fs.createWriteStream(filename));
+      counter++;
+    });
+    req.pipe(busboy);
+  }
+});
+
+var io = socketio(app);
+
+var counter = 0;
+io.on('connection', function(socket) {
+  iostream(socket).on('upload', function(stream, data) {
+    var timeStart = new Date();
+    var filename = 'socket_test_' + counter + '_' + path.basename(data.name);
+    stream.on('end', function() {
+      var timeEnd = new Date();
+      var duration = timeEnd - timeStart;
+      console.log('socket.io streaming upload complete in ' + duration + 'ms');
+      socket.disconnect();
+      fs.unlink(filename, function() {});
+    });
+    stream.pipe(fs.createWriteStream(filename));
+    counter++;
+  });
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ test.js
@@ -0,0 +1,82 @@
+var server = process.env.SERVER || 'http://localhost:3000';
+
+var async = require('async');
+var fs = require('fs');
+var request = require('request');
+var io = require('socket.io-client');
+var ss = require('socket.io-stream');
+
+var iter = process.env.ITER || 10;
+
+var filename = process.env.FILENAME || 'creepy_smile.gif';
+var stats = fs.statSync(filename);
+var fileBytes = stats.size;
+
+console.log('Starting upload test to '+server);
+console.log('File size is '+fileBytes/1000 + 'KB');
+
+var socketSummary, postSummary;
+
+async.series([
+  socketioTest,
+  postTest
+], function() {
+  console.log('\n\nSummary:');
+  console.log(socketSummary);
+  console.log(postSummary);
+});
+
+function postTest(callback) {
+  var startTime = new Date();
+  console.log('Starting HTTP POST upload test');
+  async.timesSeries(iter, function(n, callback) {
+    var start = new Date();
+    var formData = {
+      file: fs.createReadStream(filename)
+    }
+    request.post({url: server, formData: formData}, function(err, httpResponse, body) {
+      var end = new Date();
+      var time = end - start;
+      console.log('Upload [' + n + '] completed in '+time+'ms, '+(fileBytes/1000)/(time/1000)+' KB/second');
+      callback();
+    });
+  },
+  function() {
+    var endTime = new Date();
+    var totalTime = endTime - startTime;
+    console.log(iter + ' uploads completed in ' + totalTime + 'ms');
+    postSummary = 'HTTP Post average upload speed [n='+iter+']: '+ Math.round((iter*fileBytes/1000) / (totalTime / 1000)) + 'KB/second';
+    console.log(postSummary);
+    callback();
+  });
+}
+
+function socketioTest(callback) {
+  var startTime = new Date();
+  console.log('Starting socket.io streaming upload test');
+  async.timesSeries(iter, function(n, callback) {
+    var start = new Date();
+    var socket = io.connect(server, {
+      transports: ['websocket'],
+      multiplex: false
+    });
+
+    var stream = ss.createStream();
+    ss(socket).emit('upload', stream, {name: filename});
+    fs.createReadStream(filename).pipe(stream);
+    socket.on('disconnect', function() {
+      var end = new Date();
+      var time = end - start;
+      console.log('Upload [' + n + '] completed in '+time+'ms, '+(fileBytes/1000)/(time/1000)+' KB/second');
+      callback();
+    })
+  },
+  function() {
+    var endTime = new Date();
+    var totalTime = endTime - startTime;
+    console.log(iter + ' uploads completed in ' + totalTime + 'ms');
+    socketSummary = 'socket.io average upload speed [n='+iter+']: '+ Math.round((iter*fileBytes/1000) / (totalTime / 1000)) + 'KB/second';
+    console.log(socketSummary);
+    callback();
+  });
+}
